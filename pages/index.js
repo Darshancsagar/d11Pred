555aa7a39978a34cf12ec882a637fac5f24fcdf1
Darshana <darshancsagar21@gmail.com>
2023-09-02 13:35:11 +0530

    main js removed ss

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Navbar from "../components/Navbar";
-import Main from "../components/Main";
+
 import HomeDetail from "../components/HomePage/HomeDetail";
 import { getAllMatches } from "../utils/DreamPredication";
 
@@ -16,22 +16,24 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const posts = getAllMatches().map((post) => {
-    // Convert the Date object to an ISO 8601 string
-    const isoDate = post.data.date?.toISOString();
+  const posts = getAllMatches()
+    .map((post) => {
+      // Convert the Date object to an ISO 8601 string
+      const isoDate = post.data.date?.toISOString();
 
-    // Return the post with the date as a string
-    return {
-      ...post,
-      data: {
-        ...post.data,
-        date: isoDate,
-      },
-    };
-  }) .sort((a, b) => {
-    // Sort in descending order based on the date
-    return new Date(b.data.date) - new Date(a.data.date);
-  });
+      // Return the post with the date as a string
+      return {
+        ...post,
+        data: {
+          ...post.data,
+          date: isoDate,
+        },
+      };
+    })
+    .sort((a, b) => {
+      // Sort in descending order based on the date
+      return new Date(b.data.date) - new Date(a.data.date);
+    });
   return {
     props: {
       posts: posts,
