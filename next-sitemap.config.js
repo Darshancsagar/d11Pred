e996d2f96db7365b605f8ee6c7aa8b9440df5f2d
Darshana <darshancsagar21@gmail.com>
2024-07-14 14:55:42 +0530

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.dream11prediction.co.in/", // Replace with your site URL
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
